f0e3a498fb9be4d32196f42174b97075fb291dc4
Onur Gökce <76440688+Onuruno@users.noreply.github.com>
2021-12-07 22:37:29 +0300

    Update hw1.py

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -5,31 +5,36 @@ fileNames = []
 with open("InstanceNames.txt") as file:
     fileNames = (file.read().splitlines())
 
-rgbValuesq1 = []
-rgbValuesq2 = []   
-rgbValuesq3 = []    
+rgbValues = []
 for name in fileNames:
-    rgb1 = []
-    rgb2 = []
-    rgb3 = []
-    im1 = Image.open("query_1/"+name)
-    im2 = Image.open("query_2/"+name)
-    im3 = Image.open("query_3/"+name)
+    rgb = []
+    im = Image.open("support_96/"+name)
     for i in range(96):
         for j in range(96):
-            rgb1.append(im1.getpixel((i,j)))
-            rgb2.append(im2.getpixel((i,j)))
-            rgb3.append(im3.getpixel((i,j)))
-    rgbValuesq1.append(rgb1)
-    rgbValuesq2.append(rgb2)
-    rgbValuesq3.append(rgb3)
-rgbValuesq1 = np.array(rgbValuesq1)
-rgbValuesq2 = np.array(rgbValuesq2)
-rgbValuesq3 = np.array(rgbValuesq3)
+            rgb.append(im.getpixel((i,j)))
+    rgbValues.append(rgb)
+rgbValues = np.array(rgbValues)
 
-print(len(rgbValuesq1))
-print(len(rgbValuesq1[0]))
-print(len(rgbValuesq2))
-print(len(rgbValuesq2[0]))
-print(len(rgbValuesq3))
-print(len(rgbValuesq3[0]))
+Histogram128 = np.zeros(shape=(200,3,2), dtype=int)
+Histogram64 = np.zeros(shape=(200,3,4), dtype=int)
+Histogram32 = np.zeros(shape=(200,3,8), dtype=int)
+Histogram16 = np.zeros(shape=(200,3,16), dtype=int)
+
+for i in range(len(rgbValues)):
+    for pixel in range(96*96):
+        Histogram128[i][0][rgbValues[i][pixel][0]//128] +=1
+        Histogram128[i][1][rgbValues[i][pixel][1]//128] +=1
+        Histogram128[i][2][rgbValues[i][pixel][2]//128] +=1
+        
+        Histogram64[i][0][rgbValues[i][pixel][0]//64] +=1
+        Histogram64[i][1][rgbValues[i][pixel][1]//64] +=1
+        Histogram64[i][2][rgbValues[i][pixel][2]//64] +=1
+        
+        Histogram32[i][0][rgbValues[i][pixel][0]//32] +=1
+        Histogram32[i][1][rgbValues[i][pixel][1]//32] +=1
+        Histogram32[i][2][rgbValues[i][pixel][2]//32] +=1
+        
+        Histogram16[i][0][rgbValues[i][pixel][0]//16] +=1
+        Histogram16[i][1][rgbValues[i][pixel][1]//16] +=1
+        Histogram16[i][2][rgbValues[i][pixel][2]//16] +=1
+        
