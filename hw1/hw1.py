932cd01b38605abc0810a2d2c0ef7164be5a2d5e
Onur Gökce <76440688+Onuruno@users.noreply.github.com>
2021-12-10 19:00:31 +0300

    Update hw1.py

--- hw1/hw1.py
+++ hw1/hw1.py
@@ -56,6 +56,50 @@ def getHistogram(arr):
     histogram32 = np.sum(np.bincount(arr, None, 256).reshape(-1, 8), axis=1)
     return histogram2, histogram4, histogram8, histogram16, histogram32
 
+def get3dGridHistograms(arr, gridSize, numberofBins):
+    numberofgrids = (96//gridSize)*(96//gridSize)
+    binInterval = 256//numberofBins
+    histogram = np.empty([200,numberofgrids,numberofBins,numberofBins,numberofBins], dtype=int)
+    for i in range(200):
+        for j in range(numberofgrids):
+            for k in range(gridSize*gridSize):
+                pixel = arr[i][j][k]
+                x = pixel[0]//binInterval
+                y = pixel[1]//binInterval
+                z = pixel[2]//binInterval
+                histogram[i][j][x][y][z]+=1
+    return histogram
+
+def get3dGridHistogram(arr, gridSize, numberofBins):
+    numberofgrids = (96//gridSize)*(96//gridSize)
+    binInterval = 256//numberofBins
+    histogram = np.zeros([numberofgrids,numberofBins,numberofBins,numberofBins], dtype=int)
+    for j in range(numberofgrids):
+        for k in range(gridSize*gridSize):
+            pixel = arr[j][k]
+            x = pixel[0]//binInterval
+            y = pixel[1]//binInterval
+            z = pixel[2]//binInterval
+            histogram[j][x][y][z]+=1
+    return histogram
+
+def getGridHistograms(arr, gridSize, numberOfBins):
+    numberofgrids = (96//gridSize)*(96//gridSize)
+    binInterval = 256//numberOfBins
+    histogram = np.empty([200,numberofgrids,numberOfBins], dtype=int)
+    for i in range(200):
+        for j in range(numberofgrids):
+            histogram[i][j] = np.sum(np.bincount(arr[i][j], None, 256).reshape(-1, binInterval), axis=1)
+    return histogram
+
+def getGridHistogram(arr, gridSize, numberOfBins):
+    numberofgrids = (96//gridSize)*(96//gridSize)
+    binInterval = 256//numberOfBins
+    histogram = np.empty([numberofgrids,numberOfBins], dtype=int)
+    for j in range(numberofgrids):
+        histogram[j] = np.sum(np.bincount(arr[j], None, 256).reshape(-1, binInterval), axis=1)
+    return histogram
+
 def cropImage(im, size):
     images=[]
     for i in range(96//size):
@@ -63,7 +107,6 @@ def cropImage(im, size):
             images.append(im.crop((i*size,j*size,(i+1)*size,(j+1)*size)))
     return images
         
-
 def getRgbValues(im):
     rgbr = []
     rgbg = []
@@ -77,6 +120,29 @@ def getRgbValues(im):
             rgbb.append(pixel[2])
             rgb3d.append(pixel)
     return rgbr, rgbg, rgbb, rgb3d
+
+def getGridRgbValues(im, gridSize):
+    rgbR = []
+    rgbG = []
+    rgbB = []
+    rgb3D = []
+    for i in range(len(im)):
+        rgbr = []
+        rgbg = []
+        rgbb = []
+        rgb3d = []
+        for j in range(gridSize):
+            for k in range(gridSize):
+                pixel = im[i].getpixel((j,k))
+                rgbr.append(pixel[0])
+                rgbg.append(pixel[1])
+                rgbb.append(pixel[2])
+                rgb3d.append(pixel)
+        rgbR.append(rgbr)
+        rgbG.append(rgbg)
+        rgbB.append(rgbb)
+        rgb3D.append(rgb3d)
+    return rgbR, rgbG, rgbB, rgb3D
 
 def compare3dImage(source3d, im3d):
     prob3dS = source3d/9216
@@ -107,15 +173,44 @@ def compareImage(sourceR, sourceG, sourceB, imR, imG, imB):
     probBlueQ = imB/9216
     probBlueQ[probBlueQ==0] = epsilon
     
-    klDivs = (np.sum(probRedQ*np.log(probRedQ/probRedS), axis=1) + np.sum(probGreenQ*np.log(probGreenQ/probGreenS), axis=1) + np.sum(probBlueQ*np.log(probBlueQ/probBlueS)))/3
+    klDivs = (np.sum(probRedQ*np.log(probRedQ/probRedS), axis=1) + np.sum(probGreenQ*np.log(probGreenQ/probGreenS), axis=1) + np.sum(probBlueQ*np.log(probBlueQ/probBlueS), axis=1))/3
     return np.argmin(klDivs)
+
+def compareGrid3dImage(source3d, im3d, gridSize):
+    return
+
+def compareGridImage(sourceR, sourceG, sourceB, imR, imG, imB, gridSize):
+    pixelPerGrid = gridSize*gridSize
     
+    probRedS = sourceR/pixelPerGrid
+    probRedS[probRedS==0] = epsilon
+    
+    probRedQ = imR/pixelPerGrid
+    probRedQ[probRedQ==0] = epsilon
+    
+    probGreenS = sourceG/pixelPerGrid
+    probGreenS[probGreenS==0] = epsilon
+    
+    probGreenQ = imG/pixelPerGrid
+    probGreenQ[probGreenQ==0] = epsilon
+    
+    probBlueS = sourceB/pixelPerGrid
+    probBlueS[probBlueS==0] = epsilon
+    
+    probBlueQ = imB/pixelPerGrid
+    probBlueQ[probBlueQ==0] = epsilon
+    
+    klDivs = (np.sum(np.sum(probRedQ*np.log(probRedQ/probRedS), axis=2), axis=1) 
+            + np.sum(np.sum(probGreenQ*np.log(probGreenQ/probGreenS), axis=2), axis=1) 
+            + np.sum(np.sum(probBlueQ*np.log(probBlueQ/probBlueS), axis=2), axis=1))/3
+    return np.argmin(klDivs)   
 
 t1 = time.time()
 fileNames = []
 with open("InstanceNames.txt") as file:
     fileNames = (file.read().splitlines())
-
+'''
+####################################################### SINGLE GRID #######################################
 rgbRed = []
 rgbBlue = []
 rgbGreen = []
@@ -142,47 +237,7 @@ rgbGreen = np.array(rgbGreen)
 rgbBlue = np.array(rgbBlue)
 rgb3d = np.array(rgb3d)
 
-rgbRed48 = []
-rgbGreen48 = []
-rgbBlue48 = []
-rgb3d48 = []
-for name in fileNames:
-    rgb48r = [[],[],[],[]]
-    rgb48g = [[],[],[],[]]
-    rgb48b = [[],[],[],[]]
-    rgb48 = [[],[],[],[]]
-    imgrid = cropImage(Image.open("support_96/"+name), 48)
-    for i in range(4):
-        for j in range(48):
-            for k in range(48):
-                pixel = imgrid[i].getpixel((j,k))
-                rgb48[i].append(pixel)
-                rgb48r[i].append(pixel[0])
-                rgb48g[i].append(pixel[1])
-                rgb48b[i].append(pixel[2])
-    
 
-for name in fileNames:
-    rgbr = []
-    rgbg = []
-    rgbb = []
-    rgb = []
-    im = Image.open("support_96/"+name)
-    for i in range(96):
-        for j in range(96):
-            pixel=im.getpixel((i,j))
-            rgb.append(pixel)
-            rgbr.append(pixel[0])
-            rgbg.append(pixel[1])
-            rgbb.append(pixel[2])
-    rgb3d.append(rgb)
-    rgbRed.append(rgbr)
-    rgbGreen.append(rgbg)
-    rgbBlue.append(rgbb)
-
-
-
-'''
 Histogram3d128, Histogram3d64, Histogram3d32, Histogram3d16 = get3dHistograms(rgb3d)
 
 Histogram128Red, Histogram64Red, Histogram32Red, Histogram16Red, Histogram8Red = getHistograms(rgbRed)
@@ -313,7 +368,203 @@ print(q3accuricy128,q3accuricy64,q3accuricy32,q3accuricy16,q3accuricy8)
 
 print(q1_3daccuricy128,q1_3daccuricy64,q1_3daccuricy32,q1_3daccuricy16)
 print(q2_3daccuricy128,q2_3daccuricy64,q2_3daccuricy32,q2_3daccuricy16)
-print(q3_3daccuricy128,q3_3daccuricy64,q3_3daccuricy32,q3_3daccuricy16)'''
+print(q3_3daccuricy128,q3_3daccuricy64,q3_3daccuricy32,q3_3daccuricy16)
+'''
+##################################### 48x48 GRID ####################################
+rgbRed48 = []
+rgbGreen48 = []
+rgbBlue48 = []
+rgb3d48 = []
+for name in fileNames:
+    rgb48r = []
+    rgb48g = []
+    rgb48b = []
+    rgb48 = []
+    imgrid = cropImage(Image.open("support_96/"+name), 48)
+    for i in imgrid:
+        subImageR = []
+        subImageG = []
+        subImageB = []
+        subImage = []
+        for j in range(48):
+            for k in range(48):
+                pixel = i.getpixel((j,k))
+                subImageR.append(pixel[0])
+                subImageG.append(pixel[1])
+                subImageB.append(pixel[2])
+                subImage.append(pixel)
+        rgb48r.append(subImageR)
+        rgb48g.append(subImageG)
+        rgb48b.append(subImageB)
+        rgb48.append(subImage)
+    rgbRed48.append(rgb48r)
+    rgbGreen48.append(rgb48g)
+    rgbBlue48.append(rgb48b)
+    rgb3d48.append(rgb48)
+rgbRed48 = np.array(rgbRed48)
+rgbGreen48 = np.array(rgbGreen48)
+rgbBlue48 = np.array(rgbBlue48)
+rgb3d48 = np.array(rgb3d48)
+
+GridHistogram48Red = getGridHistograms(rgbRed48, 48, 8)
+GridHistogram48Green = getGridHistograms(rgbGreen48, 48, 8)
+GridHistogram48Blue = getGridHistograms(rgbBlue48, 48, 8)
+GridHistogram3D48 = get3dGridHistograms(rgb3d48, 48, 4)
+
+q1acc48=q2acc48=q3acc48=0
+q13Dacc48=q23Dacc48=q33Dacc48=0
+
+for i in range(200):
+    image1 = cropImage(Image.open("query_1/"+fileNames[i]), 48)
+    rgbR, rgbG, rgbB, rgb3D = getGridRgbValues(image1, 48)
+    h48r = getGridHistogram(rgbR, 48, 8)
+    h48g = getGridHistogram(rgbG, 48, 8)
+    h48b = getGridHistogram(rgbB, 48, 8)
+    h48 = get3dGridHistogram(rgb3D, 48, 4)
+    if(i == compareGridImage(GridHistogram48Red, GridHistogram48Green, GridHistogram48Blue, h48r, h48g, h48b, 48)):
+        q1acc48+=1
+    if(i == compareGrid3dImage(GridHistogram3D48, h48, 48)):
+        q13Dacc48+=1
+        
+    image1 = cropImage(Image.open("query_2/"+fileNames[i]), 48)
+    rgbR, rgbG, rgbB, rgb3D = getGridRgbValues(image1, 48)
+    h48r = getGridHistogram(rgbR, 48, 8)
+    h48g = getGridHistogram(rgbG, 48, 8)
+    h48b = getGridHistogram(rgbB, 48, 8)
+    h48 = get3dGridHistogram(rgb3D, 48, 4)
+    if(i == compareGridImage(GridHistogram48Red, GridHistogram48Green, GridHistogram48Blue, h48r, h48g, h48b, 48)):
+        q2acc48+=1
+    if(i == compareGrid3dImage(GridHistogram3D48, h48, 48)):
+        q23Dacc48+=1
+        
+    image1 = cropImage(Image.open("query_3/"+fileNames[i]), 48)
+    rgbR, rgbG, rgbB, rgb3D = getGridRgbValues(image1, 48)
+    h48r = getGridHistogram(rgbR, 48, 8)
+    h48g = getGridHistogram(rgbG, 48, 8)
+    h48b = getGridHistogram(rgbB, 48, 8)
+    h48 = get3dGridHistogram(rgb3D, 48, 4)
+    if(i == compareGridImage(GridHistogram48Red, GridHistogram48Green, GridHistogram48Blue, h48r, h48g, h48b, 48)):
+        q3acc48+=1
+    if(i == compareGrid3dImage(GridHistogram3D48, h48, 48)):
+        q33Dacc48+=1
+q1acc48/=200
+q2acc48/=200
+q3acc48/=200
+q13Dacc48/=200
+q23Dacc48/=200
+q33Dacc48/=200        
+
+print(q1acc48, q2acc48, q3acc48)        
+
+'''
+##################################### 24x24 GRID ####################################
+rgbRed24 = []
+rgbGreen24 = []
+rgbBlue24 = []
+rgb3d24 = []
+for name in fileNames:
+    rgb24r = []
+    rgb24g = []
+    rgb24b = []
+    rgb24 = []
+    imgrid = cropImage(Image.open("support_96/"+name), 24)
+    for i in imgrid:
+        subImageR = []
+        subImageG = []
+        subImageB = []
+        subImage = []
+        for j in range(24):
+            for k in range(24):
+                pixel = i.getpixel((j,k))
+                subImageR.append(pixel[0])
+                subImageG.append(pixel[1])
+                subImageB.append(pixel[2])
+                subImage.append(pixel)
+        rgb24r.append(subImageR)
+        rgb24g.append(subImageG)
+        rgb24b.append(subImageB)
+        rgb24.append(subImage)
+    rgbRed24.append(rgb24r)
+    rgbGreen24.append(rgb24g)
+    rgbBlue24.append(rgb24b)
+    rgb3d24.append(rgb24)
+rgbRed24 = np.array(rgbRed24)
+rgbGreen24 = np.array(rgbGreen24)
+rgbBlue24 = np.array(rgbBlue24)
+rgb3d24 = np.array(rgb3d24)
+
+##################################### 16x16 GRID ####################################
+rgbRed16 = []
+rgbGreen16 = []
+rgbBlue16 = []
+rgb3d16 = []
+for name in fileNames:
+    rgb16r = []
+    rgb16g = []
+    rgb16b = []
+    rgb16 = []
+    imgrid = cropImage(Image.open("support_96/"+name), 16)
+    for i in imgrid:
+        subImageR = []
+        subImageG = []
+        subImageB = []
+        subImage = []
+        for j in range(16):
+            for k in range(16):
+                pixel = i.getpixel((j,k))
+                subImageR.append(pixel[0])
+                subImageG.append(pixel[1])
+                subImageB.append(pixel[2])
+                subImage.append(pixel)
+        rgb16r.append(subImageR)
+        rgb16g.append(subImageG)
+        rgb16b.append(subImageB)
+        rgb16.append(subImage)
+    rgbRed16.append(rgb16r)
+    rgbGreen16.append(rgb16g)
+    rgbBlue16.append(rgb16b)
+    rgb3d16.append(rgb16)
+rgbRed16 = np.array(rgbRed16)
+rgbGreen16 = np.array(rgbGreen16)
+rgbBlue16 = np.array(rgbBlue16)
+rgb3d16 = np.array(rgb3d16)
+
+
+##################################### 12x12 GRID ####################################
+rgbRed12 = []
+rgbGreen12 = []
+rgbBlue12 = []
+rgb3d12 = []
+for name in fileNames:
+    rgb12r = []
+    rgb12g = []
+    rgb12b = []
+    rgb12 = []
+    imgrid = cropImage(Image.open("support_96/"+name), 12)
+    for i in imgrid:
+        subImageR = []
+        subImageG = []
+        subImageB = []
+        subImage = []
+        for j in range(12):
+            for k in range(12):
+                pixel = i.getpixel((j,k))
+                subImageR.append(pixel[0])
+                subImageG.append(pixel[1])
+                subImageB.append(pixel[2])
+                subImage.append(pixel)
+        rgb12r.append(subImageR)
+        rgb12g.append(subImageG)
+        rgb12b.append(subImageB)
+        rgb12.append(subImage)
+    rgbRed12.append(rgb12r)
+    rgbGreen12.append(rgb12g)
+    rgbBlue12.append(rgb12b)
+    rgb3d12.append(rgb12)
+rgbRed12 = np.array(rgbRed12)
+rgbGreen12 = np.array(rgbGreen12)
+rgbBlue12 = np.array(rgbBlue12)
+rgb3d12 = np.array(rgb3d12)'''
 
 
 t2=time.time()
